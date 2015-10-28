d823719cbb754f73d4b7b2b54d63f8e57795dbfd
Babble- <david.bernhard@student.htldornbirn.at>
2015-10-28 16:56:14 +0100

    Added some UserControls

---
+++ MySoundLib/UserControlSongs.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MySoundLib
+{
+	/// <summary>
+	/// Interaction logic for UserControlSongs.xaml
+	/// </summary>
+	public partial class UserControlSongs : UserControl
+	{
+		private readonly ServerConnectionManager _serverConnectionManager;
+
+		public UserControlSongs(ServerConnectionManager connectionManager)
+		{
+			InitializeComponent();
+			_serverConnectionManager = connectionManager;
+
+			var songs = _serverConnectionManager.GetDataTable("select * from songs s left join artists a on (a.artist_id = s.artist)");
+
+			ListViewSongs.ItemsSource = songs.DefaultView;
+		}
+
+		private void ButtonAddNewSong_Click(object sender, RoutedEventArgs e)
+		{
+			var uiElementCollection = ((Grid) Parent).Children;
+
+			uiElementCollection.Clear();
+			uiElementCollection.Add(new UserControlUploadSong(_serverConnectionManager));
+		}
+	}
+}
---
+++ MySoundLib/UserControlUploadSong.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System.Data;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace MySoundLib
+{
+	/// <summary>
+	/// Interaction logic for UserControlUploadSong.xaml
+	/// </summary>
+	public partial class UserControlUploadSong : UserControl
+	{
+		private readonly ServerConnectionManager _connectionManager;
+		private string _filePath;
+		
+		public UserControlUploadSong(ServerConnectionManager connectionManager)
+		{
+			InitializeComponent();
+			_connectionManager = connectionManager;
+		}
+
+		private void UserControl_Loaded(object sender, RoutedEventArgs e)
+		{
+			var artists = _connectionManager.GetDataTable("select artist_name from artists");
+
+			foreach (DataRow row in artists.Rows)
+			{
+				ComboBoxArtist.Items.Add(row[0]);
+			}
+
+			var genres = _connectionManager.GetDataTable("select genre_name from genres");
+
+			foreach (DataRow row in genres.Rows)
+			{
+				ComboBoxGenre.Items.Add(row[0]);
+			}
+		}
+
+		private void ButtonSelectFile_Click(object sender, RoutedEventArgs e)
+		{
+			var dlg = new Microsoft.Win32.OpenFileDialog
+			{
+				DefaultExt = ".mp3",
+				Filter = "MP3 Files (*.mp3)|*.mp3|MPEG 4 Audio (*.m4a)|*.m4a"
+			};
+
+			var result = dlg.ShowDialog();
+
+			if (result == true)
+			{
+				_filePath = dlg.FileName;
+
+				ButtonSelectFile.Content = System.IO.Path.GetFileName(_filePath);
+			}
+			else
+			{
+				MessageBox.Show("No path choosen");
+			}
+		}
+
+		private void ButtonAddSong_Click(object sender, RoutedEventArgs e)
+		{
+			var fs = new FileStream(_filePath, FileMode.Open, FileAccess.Read);
+			var br = new BinaryReader(fs);
+			var imageData = br.ReadBytes((int)fs.Length);
+			br.Close();
+			fs.Close();
+
+			var command = new MySqlCommand("INSERT INTO Songs(song_title, track) VALUES(@title, @track)");
+
+			command.Parameters.AddWithValue("@title", TextBoxSongTitle.Text);
+			command.Parameters.AddWithValue("@track", imageData);
+
+			_connectionManager.ExecuteCommand(command);
+		}
+	}
+}

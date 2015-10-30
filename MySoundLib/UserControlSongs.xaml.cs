ef4a71e976b6ef9c9f07e3d39cfe58ccbd36b3df
Babble- <david.bernhard@student.htldornbirn.at>
2015-10-30 01:21:05 +0100

    Song-List: show more information

--- MySoundLib/UserControlSongs.xaml.cs
+++ MySoundLib/UserControlSongs.xaml.cs
@@ -15,7 +15,7 @@ namespace MySoundLib
 			InitializeComponent();
 			_serverConnectionManager = connectionManager;
 
-			var songs = _serverConnectionManager.GetDataTable("select song_title, artist_name from songs s left join artists a on (a.artist_id = s.artist)");
+			var songs = _serverConnectionManager.GetDataTable("select song_id, song_title, artist_name, album_name, genre_name, length from songs s left join artists a on (a.artist_id = s.artist) left join genres g on (s.genre = g.genre_id) left join albums al on (al.album_id = s.album)");
 
 			ListViewSongs.ItemsSource = songs.DefaultView;
 		}

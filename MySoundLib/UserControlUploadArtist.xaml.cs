c3ae9f993f76ae0d689890c7e5b9c69547621f03
Babble- <david.bernhard@student.htldornbirn.at>
2015-12-18 12:11:58 +0100

    formatting

--- MySoundLib/UserControlUploadArtist.xaml.cs
+++ MySoundLib/UserControlUploadArtist.xaml.cs
@@ -5,51 +5,56 @@ using System.Windows.Controls;
 
 namespace MySoundLib
 {
-	/// <summary>
-	/// Interaction logic for UserControlUploadArtist.xaml
-	/// </summary>
-	public partial class UserControlUploadArtist : UserControl
-	{
-		private readonly MainWindow _mainWindow;
-		ServerConnectionManager _connectionManager {
+    /// <summary>
+    /// Interaction logic for UserControlUploadArtist.xaml
+    /// </summary>
+    public partial class UserControlUploadArtist : UserControl
+    {
+        private readonly MainWindow _mainWindow;
+        ServerConnectionManager _connectionManager
+        {
             get
             {
                 return _mainWindow.ConnectionManager;
             }
         }
 
-		public UserControlUploadArtist(MainWindow mainWindow)
-		{
-			InitializeComponent();
-			_mainWindow = mainWindow;
-			TextBoxArtistName.Focus();
-		}
+        public UserControlUploadArtist(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            _mainWindow = mainWindow;
+            TextBoxArtistName.Focus();
+        }
 
-		private void ButtonCancel_Click(object sender, RoutedEventArgs e)
-		{
-			ShowArtists();
-		}
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            ShowArtists();
+        }
 
-		private void ButtonAddArtist_Click(object sender, RoutedEventArgs e)
-		{
-			if (string.IsNullOrEmpty(TextBoxArtistName.Text)) {
-				MessageBox.Show("Please insert name");
-				return;
-			}
-			int result = _connectionManager.ExecuteCommand(CommandFactory.InsertNewArtist(TextBoxArtistName.Text));
+        private void ButtonAddArtist_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(TextBoxArtistName.Text))
+            {
+                MessageBox.Show("Please insert name");
+                return;
+            }
+            int result = _connectionManager.ExecuteCommand(CommandFactory.InsertNewArtist(TextBoxArtistName.Text));
 
-			if (result != 1) {
-				Debug.WriteLine("Unable to insert artist: " + TextBoxArtistName.Text);
-			}
-			if (result == 1) {
-				ShowArtists();
-			}
-		}
+            if (result != 1)
+            {
+                Debug.WriteLine("Unable to insert artist: " + TextBoxArtistName.Text);
+            }
+            if (result == 1)
+            {
+                ShowArtists();
+            }
+        }
 
-		private void ShowArtists() {
-			_mainWindow.GridContent.Children.Clear();
-			_mainWindow.ListBoxCategory.SelectedIndex = 2;
-			_mainWindow.GridContent.Children.Add(new UserControlArtists(_mainWindow));
-		}
-	}
+        private void ShowArtists()
+        {
+            _mainWindow.GridContent.Children.Clear();
+            _mainWindow.ListBoxCategory.SelectedIndex = 2;
+            _mainWindow.GridContent.Children.Add(new UserControlArtists(_mainWindow));
+        }
+    }
 }

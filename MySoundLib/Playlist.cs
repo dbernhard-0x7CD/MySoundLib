4a86bf1d817756470835d66590fa2506e1ea6729
Babble- <david.bernhard@student.htldornbirn.at>
2016-01-21 00:11:57 +0100

    Started implementing playlist

---
+++ MySoundLib/Playlist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySoundLib
+{
+    public class Playlist
+    {
+        public string Name { get; set; }
+        public int Id;
+        int currentIndex { get; set; }
+        List<int> songIds;
+
+        public void Next()
+        {
+            currentIndex++;
+        }
+    }
+}
